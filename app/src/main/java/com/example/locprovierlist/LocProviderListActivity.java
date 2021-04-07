dc22f442872f2d402cc91d0a08809949c14b9c90
ByunYein <dpdls311@gmail.com>
2021-04-07 15:18:57 +0900

    added Button

--- app/src/main/java/com/example/locprovierlist/LocProviderListActivity.java
+++ app/src/main/java/com/example/locprovierlist/LocProviderListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,6 +15,7 @@ public class LocProviderListActivity extends AppCompatActivity {
     TextView mTextView;
     LocationManager locMgr;
     List<String> locProviders;
+    Button mbutton;
 
 
     @Override
@@ -24,12 +27,21 @@ public class LocProviderListActivity extends AppCompatActivity {
         locMgr=(LocationManager)getSystemService(LOCATION_SERVICE);
         locProviders=locMgr.getAllProviders();
 
+        mbutton=findViewById(R.id.button);
+
         String s = "";
         for(int i=0;i<locProviders.size();i++){
             s+="Loc. Provider"+locProviders.get(i)+"\n"
                     +"Status:"+locMgr.isProviderEnabled(locProviders.get(i))+"\n\n";
         }
-        mTextView.setText(s);
+
+
+        mbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTextView.setText(s);
+            }
+        });
 
     }
 }
